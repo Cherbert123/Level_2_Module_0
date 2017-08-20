235f0d3d737672beaa7b5467d921266e4472ac1f
Charlie <league@WTS21.attlocal.net>
2017-08-20 11:15:13 -0700

    Arrays

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,15 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +18,48 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	ArrayList<String> people = new ArrayList();
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+	JButton AddButton = new JButton();
+	JButton ViewButton = new JButton();
+	JButton RemoveButton = new JButton();
+	String list = "";
+	public static void main(String[] args){
+		GuestBook run = new GuestBook();
+		run.Buttons();
+		
+	}
+	public void Buttons() {
+		AddButton.setText("Add Guest");
+		ViewButton.setText("View List");
+		RemoveButton.setText("Remove");
+		AddButton.addActionListener(this);
+		ViewButton.addActionListener(this);
+		RemoveButton.addActionListener(this);
+		frame.add(panel);
+		frame.setVisible(true);
+		panel.add(AddButton);
+		panel.add(ViewButton);
+		panel.add(RemoveButton);
+		frame.pack();
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == AddButton) {
+			people.add(JOptionPane.showInputDialog("Enter A Name:"));
+		}
+		if(e.getSource() == ViewButton) {
+			list = "";
+			for(int i = 1; i < people.size() + 1; i++){
+				list = list + "Guest #" + i + " " + people.get(i - 1) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, list);
+		}
+		if(e.getSource() == RemoveButton) {
+			people.remove(Integer.parseInt(JOptionPane.showInputDialog("Enter Guest Number:")) - 1);
+		}
+		
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,10 +1,20 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> Strings = new ArrayList();
+		Strings.add("Popcorn"); Strings.add("Chips"); Strings.add("Preztel"); Strings.add("Hotdog"); Strings.add("Hamburger");
+		for(int i = 0; i < Strings.size(); i++){System.out.println(Strings.get(i));}
+		System.out.println("");
+		for(int i = 0; i < Strings.size(); i++){if(i%2 == 1){System.out.println(Strings.get(i));}}
+		System.out.println("");
+		for(int i = Strings.size(); i > 0; i--){System.out.println(Strings.get(i - 1));}
+		System.out.println("");
+		for(int i = 0; i < Strings.size(); i++){if(Strings.get(i).contains("e")){System.out.println(Strings.get(i));}}
 		//2. Add five Strings to your list
 		
 		//3. Print all the Strings using a standard for-loop
